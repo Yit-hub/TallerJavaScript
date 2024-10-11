8b57acf768c9b0c00521f06926552ec6ea87fbe1
Yit-hub <yidanielchen.23@campuscamara.es>
2024-10-11 23:02:33 +0200

    cambio

--- Carrito.js
+++ Carrito.js
@@ -1,4 +1,14 @@
-//Variable global
+// Gestión de un Carrito de Compras
+// ● Descripción: Implementa un sistema de carrito de compras donde se puedan
+// agregar productos con un nombre, precio y cantidad. El programa debe calcular el
+// total del carrito y mostrar todos los productos en el carrito utilizando funciones.
+// ● Requisitos:
+// ○ Usa var, let, y const para las variables adecuadas.
+// ○ Usa estructuras de control como if-else para verificar si el producto ya
+// está en el carrito.
+// ○ Utiliza un bucle for para mostrar todos los productos y calcular el total.
+// ● Objetivo: Practicar el uso de estructuras, funciones y control de flujo en una
+// situación más cercana a la vida real.
 let carrito = [];
 
 function agregarProducto(nombre, precio, cantidad, tipoIva) {
