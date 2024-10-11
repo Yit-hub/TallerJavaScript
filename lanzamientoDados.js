80d4bbe8cb7bb12dfc8b80126d6db6450b099077
Yit-hub <yidanielchen.23@campuscamara.es>
2024-10-11 23:04:48 +0200

    a

--- lanzamientoDados.js
+++ lanzamientoDados.js
@@ -1,3 +1,10 @@
+// Simulación de Lanzamiento de Dados 
+// ● Descripción: Crea una función que simule el lanzamiento de dos dados (números aleatorios entre 1 y 6). El programa debe contar cuántas veces se obtiene un total de 7 al lanzar los dados 100 veces. 
+// ● Requisitos: 
+// ○ Usa un bucle for para realizar las simulaciones. 
+// ○ Utiliza Math.random() para generar los números aleatorios. 
+// ○ Usa una función para encapsular la lógica del lanzamiento de dados. ● Objetivo: Trabajar con números aleatorios, bucles y funciones. 
+
 function lanzamiento(){
     function tirarDados(){
         return Math.floor(Math.random() * 6) + 1;
